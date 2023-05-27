845dda5ec54891bd5503436e4bbfd49e2dab433c
RobertaNechita <roberta.nechita1610@gmail.com>
2023-05-27 18:58:52 +0300

    [report] BaseSettings comments for java doc added

--- Project/SchoolTimetable/src/main/java/org/presentation/swing/BaseSettings.java
+++ Project/SchoolTimetable/src/main/java/org/presentation/swing/BaseSettings.java
@@ -1,7 +1,10 @@
 package org.presentation.swing;
 
 import javax.swing.*;
-
+/**
+ * The BaseSettings interface represents the base settings for a specific component in the Timetable Generator application.
+ * It defines methods to create JPanels for the main panel and the left and right panels of the component.
+ */
 public interface BaseSettings {
     JPanel createJPanel(JPanel main, String labelText);
 
