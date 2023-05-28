424ae1ce985db064af9bf2c4b3df8102a8846e99
Gheorghita Mutu <gheorghitamutu@gmail.com>
2023-05-28 20:51:10 +0300

    [Project][Docs] + added javadoc comments for repositories (2)

--- Project/SchoolTimetable/src/main/java/org/dataaccess/database/MainDatabaseHibernateProvider.java
+++ Project/SchoolTimetable/src/main/java/org/dataaccess/database/MainDatabaseHibernateProvider.java
@@ -4,11 +4,25 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+/**
+ * This is the class for MainDatabaseHibernateProvider.
+ */
 public class MainDatabaseHibernateProvider implements IHibernateProvider {
 
+    /**
+     * This is the entity manager factory.
+     */
     public static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence-unit");
+
+    /**
+     * This is the entity manager.
+     */
     public EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+    /**
+     * This is the method to get the entity manager.
+     * @return The entity manager.
+     */
     @Override
     public EntityManager getEntityManager() {
 
